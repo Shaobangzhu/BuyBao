77a637896d29a46806425b45efaba6fd45dd457e
hongshaoji <shaobangzhuwebtech2017@gmail.com>
2018-02-26 17:41:50 -0800

    Finish Statistic Website

--- src/views/OrderSuccess.vue
+++ src/views/OrderSuccess.vue
@@ -31,96 +31,34 @@
         </div>
       </div>
     </header>
-    <div class="nav-breadcrumb-wrap">
-      <div class="container">
-        <nav class="nav-breadcrumb">
-          <a href="/">Home</a>
-          <span>Goods</span>
-        </nav>
+    <div class="container">
+      <div class="page-title-normal">
+        <h2 class="page-title-h2"><span>check out</span></h2>
       </div>
-    </div>
-    <div class="accessory-result-page accessory-page">
-      <div class="container">
-        <div class="filter-nav">
-          <span class="sortby">Sort by:</span>
-          <a href="javascript:void(0)" class="default cur">Default</a>
-          <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-          <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
-        </div>
-        <div class="accessory-result">
-          <!-- filter -->
-          <div class="filter stopPop" id="filter">
-            <dl class="filter-price">
-              <dt>Price:</dt>
-              <dd><a href="javascript:void(0)">All</a></dd>
-              <dd>
-                <a href="javascript:void(0)">0 - 100</a>
-              </dd>
-              <dd>
-                <a href="javascript:void(0)">100 - 500</a>
-              </dd>
-              <dd>
-                <a href="javascript:void(0)">500 - 1000</a>
-              </dd>
-              <dd>
-                <a href="javascript:void(0)">1000 - 2000</a>
-              </dd>
-            </dl>
-          </div>
+      <!-- 进度条 -->
+      <div class="check-step">
+        <ul>
+          <li class="cur"><span>Confirm</span> address</li>
+          <li class="cur"><span>View your</span> order</li>
+          <li class="cur"><span>Make</span> payment</li>
+          <li class="cur"><span>Order</span> confirmation</li>
+        </ul>
+      </div>
 
-          <!-- search result accessories list -->
-          <div class="accessory-list-wrap">
-            <div class="accessory-list col-4">
-              <ul>
-                <li>
-                  <div class="pic">
-                    <a href="#"><img src="static/1.jpg" alt=""></a>
-                  </div>
-                  <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">999</div>
-                    <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="pic">
-                    <a href="#"><img src="static/2.jpg" alt=""></a>
-                  </div>
-                  <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">1000</div>
-                    <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="pic">
-                    <a href="#"><img src="static/3.jpg" alt=""></a>
-                  </div>
-                  <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">500</div>
-                    <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="pic">
-                    <a href="#"><img src="static/4.jpg" alt=""></a>
-                  </div>
-                  <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">2499</div>
-                    <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                    </div>
-                  </div>
-                </li>
-              </ul>
+      <div class="order-create">
+        <div class="order-create-pic"><img src="/static/ok-2.png" alt=""></div>
+        <div class="order-create-main">
+          <h3>Congratulations! <br>Your order is under processing!</h3>
+          <p>
+            <span>Order ID：62000000000</span>
+            <span>Order total：5230</span>
+          </p>
+          <div class="order-create-btn-wrap">
+            <div class="btn-l-wrap">
+              <a class="btn btn--m">Cart List</a>
+            </div>
+            <div class="btn-r-wrap">
+              <a class="btn btn--m">Goods List</a>
             </div>
           </div>
         </div>
@@ -159,7 +97,7 @@
 <script>
   export default{
     data(){
-      return {
+      return{
 
       }
     }
